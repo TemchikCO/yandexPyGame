32a28a916b7e134eb35351982dbe056266394874
Pupsik192 <sepavita@gmail.com>
2021-11-23 17:22:26 +0300

    Виктор(#2)

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+
+from random import randrange
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap, QColor, QPainter
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QFileDialog, QSlider
+
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.smile(qp)
+            qp.end()
+
+    def smile(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(100, 100, randrange(300), randrange(300))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
