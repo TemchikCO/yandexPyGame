122684158faed5dbc6a327f688a115afe08fd0e6
TemchikCO <tema.tatarinov@gmal.com>
2021-11-23 17:37:48 +0300

    Артём

--- main.py
+++ main.py
@@ -7,32 +7,27 @@ from PyQt5.QtGui import QPixmap, QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QFileDialog, QSlider
 
 
-
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.paint_flag = False
         uic.loadUi('UI.ui', self)
-        self.do_paint = False
         self.initUI()
 
     def initUI(self):
         self.pushButton.clicked.connect(self.paint)
 
     def paint(self):
-        self.do_paint = True
+        self.paint_flag = True
         self.repaint()
 
     def paintEvent(self, event):
-        if self.do_paint:
+        if self.paint_flag:
             qp = QPainter()
             qp.begin(self)
-            self.smile(qp)
+            qp.setBrush(QColor(255, 255, 0))
+            qp.drawEllipse(randrange(300), randrange(300), 100, 100)
             qp.end()
-
-    def smile(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        x = randrange(300)
-        qp.drawEllipse(100, 100, x, x)
 
 
 if __name__ == '__main__':
