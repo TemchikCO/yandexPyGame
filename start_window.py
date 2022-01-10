2e00c97bfe9f71243dad432b3e8a8498b0c68724
TemchikCO <92385184+TemchikCO@users.noreply.github.com>
2022-01-10 23:58:59 +0300

    Add files via upload

--- start_window.py
+++ start_window.py
@@ -3,7 +3,7 @@ import random
 import pygame
 import data
 from pygame.sprite import Sprite
-from data import all_sprites
+from data import all_sprites, trees, clouds, start_sprites, play_sprites
 
 button_pressed = False
 GRAVITY = 1
@@ -94,6 +94,8 @@ class Cloud(StartSprite):
     def __init__(self, group, surface, x, y):
         super().__init__(group, surface, x, y)
         self.frames = []
+        self.add(start_sprites)
+        self.add(clouds)
         for i in range(12):
             animate_cloud_image1 = data.load_image(f'clouds/{i + 1}.png', -1)
             animate_cloud_image = pygame.transform.scale(animate_cloud_image1, (200, 150))
@@ -109,6 +111,37 @@ class Cloud(StartSprite):
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[self.cur_frame]
             self.upgrades_count = 0
+
+
+class Tree(StartSprite):
+    def __init__(self, group, surface, x, y):
+        super().__init__(group, surface, x, y)
+        self.frames = []
+        self.speed = 500
+        self.add(start_sprites)
+        self.add(trees)
+        for i in range(20):
+            animate_tree_image1 = data.load_image(f'tree_gif/{i + 1}.png', -1)
+            animate_tree_image = pygame.transform.scale(animate_tree_image1, (200, 150))
+            self.frames.append(animate_tree_image)
+        self.upgrades_count = 1
+        self.upgrades_per_frame = 6
+        self.cur_frame = 0
+        self.image = surface
+
+    def update(self, ms, horizontal, down_flag):
+        self.upgrades_count += 1
+        self.speed += ms // 1000
+        if self.upgrades_count == self.upgrades_per_frame:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+            self.upgrades_count = 0
+        if not down_flag:
+            self.x -= (self.speed * ms / 1000 + horizontal * self.speed * ms / 2000)
+        else:
+            self.x -= self.speed * ms / 1000
+        if self.x <= -382:
+            self.x = 1200
 
 
 class AnimatedSprite(StartSprite):
@@ -141,9 +174,11 @@ class Raner_hero(StartSprite):
         self.jumping_frames_forward = []
         self.ranning_frames_back = []
         self.jumping_frames_back = []
+        self.down_frames = []
         self.active_frames = []
         self.frames = []
-        self.jump_force = 70
+        self.jump_force = 150
+        self.down_flag = False
         for i in range(8):
             animate_ran_hero_image1 = data.load_image(f'{filename}/{i + 1}.png', -1)
             animate_ran_hero_image = pygame.transform.scale(animate_ran_hero_image1, (96, 111))
@@ -160,6 +195,10 @@ class Raner_hero(StartSprite):
             animate_jump_hero_image1 = data.load_image(f'jumping_hero_mirred/{i + 1}.png', -1)
             animate_jump_hero_image = pygame.transform.scale(animate_jump_hero_image1, (96, 111))
             self.jumping_frames_back.append(animate_jump_hero_image)
+        for i in range(3):
+            animate_down_hero_image1 = data.load_image(f'down_hero/{i + 1}.png', -1)
+            animate_down_hero_image = pygame.transform.scale(animate_down_hero_image1, (96, 111))
+            self.down_frames.append(animate_down_hero_image)
         self.upgrades_count = 1
         self.upgrades_per_frame = 7
         self.cur_frame = 0
@@ -169,13 +208,18 @@ class Raner_hero(StartSprite):
         self.upgrades_count += 1
         if self.upgrades_count == self.upgrades_per_frame:
             if self.is_grounded():
-                if self.dir_x >= 0:
-                    self.active_frames = self.ranning_frames_forward.copy()
+                if self.down_flag:
+                    self.active_frames = self.down_frames.copy()
                 else:
-                    self.active_frames = self.ranning_frames_back.copy()
+                    if self.dir_x >= 0:
+                        self.active_frames = self.ranning_frames_forward.copy()
+                    else:
+                        self.active_frames = self.ranning_frames_back.copy()
                 self.cur_frame = (self.cur_frame + 1) % len(self.active_frames)
                 self.image = self.active_frames[self.cur_frame]
             elif not self.is_grounded():
+                if self.down_flag:
+                    data.gravity = 20
                 if self.dir_x >= 0:
                     self.active_frames = self.jumping_frames_forward.copy()
                 else:
@@ -184,19 +228,23 @@ class Raner_hero(StartSprite):
                 self.image = self.active_frames[self.cur_frame]
             self.upgrades_count = 0
         self.x += self.dir_x * ms / 1000
+        if self.x <= 0:
+            self.x -= self.dir_x * ms / 1000
+
         self.y += self.dir_y * ms / 200
         if self.is_grounded():
             self.dir_y = 0
             self.can_jump = True
         else:
             self.can_jump = False
-            self.dir_y += data.gravity * ms / 100
+            self.dir_y += data.gravity * ms / 15
 
     def is_grounded(self):
         return self.y > 370
 
     def set_move(self, horizontal):
-        self.dir_x = horizontal * self.speed
+        if not self.down_flag:
+            self.dir_x = horizontal * self.speed
 
     def jump(self):
         if self.can_jump:
@@ -231,13 +279,33 @@ class Raner_hero(StartSprite):
 #             self.upgrades_count = 0
 
 
+class Count(Sprite):
+    def __init__(self, group):
+        super(Count, self).__init__()
+        self.time = 0
+        self.group = group
+        self.count = 0
+        self.koef = 1
+
+    def draw(self, screen):
+        font = pygame.font.Font(None, 50)
+        text = font.render(f"{int(self.count)}", True, 'black')
+        text_x = 1100 - text.get_width() // 2
+        text_y = text.get_height() // 2
+        screen.blit(text, (text_x, text_y))
+
+    def update(self, ms):
+        self.koef += 0.0005
+        self.time += ms
+        self.count = self.time * self.koef
+
+
 def main():
     clock = pygame.time.Clock()
     FPS = 120
     pygame.display.set_caption('Добро пожаловать в игру')
-    clouds = pygame.sprite.Group()
-    start_sprites = pygame.sprite.Group()
-    play_sprites = pygame.sprite.Group()
+
+    count = Count(all_sprites)
 
     ground_image1 = data.load_image("Ground.jpg")
     ground_image = pygame.transform.scale(ground_image1, (1200, 700))
@@ -253,28 +321,29 @@ def main():
     cloud_image = pygame.transform.scale(cloud_image1, (200, 150))
 
     cloud1 = Cloud(all_sprites, cloud_image, 0, 100)
-    cloud1.add(clouds)
-    cloud1.add(start_sprites)
 
     cloud2 = Cloud(all_sprites, cloud_image, 150, 50)
-    cloud2.add(clouds)
-    cloud2.add(start_sprites)
 
     cloud3 = Cloud(all_sprites, cloud_image, 340, 0)
-    cloud3.add(clouds)
-    cloud3.add(start_sprites)
 
     cloud4 = Cloud(all_sprites, cloud_image, 660, 0)
-    cloud4.add(clouds)
-    cloud4.add(start_sprites)
 
     cloud5 = Cloud(all_sprites, cloud_image, 850, 50)
-    cloud5.add(clouds)
-    cloud5.add(start_sprites)
 
     cloud6 = Cloud(all_sprites, cloud_image, 1000, 100)
-    cloud6.add(clouds)
-    cloud6.add(start_sprites)
+
+    tree_image1 = data.load_image("tree_gif/1.png", -1)
+    tree_image = pygame.transform.scale(tree_image1, (200, 150))
+
+    tree1 = Tree(all_sprites, tree_image, 0, 252)
+
+    tree2 = Tree(all_sprites, tree_image, 300, 252)
+
+    tree3 = Tree(all_sprites, tree_image, 600, 252)
+
+    tree4 = Tree(all_sprites, tree_image, 900, 252)
+
+    tree5 = Tree(all_sprites, tree_image, 1200, 252)
 
     avatar_image1 = data.load_image("avatar.png", -1)
     avatar_image = pygame.transform.scale(avatar_image1, (400, 400))
@@ -297,6 +366,7 @@ def main():
 
     running = True
     while running:
+        pressed = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -324,6 +394,7 @@ def main():
         avatar.update(clock.tick(FPS))
         start_button.update(clock.tick(FPS))
         clouds.update()
+        horizontal = int(pressed[pygame.K_d]) - int(pressed[pygame.K_a])
         if avatar.rect.topleft[1] <= -300:
             avatar.kill()
         if start_button.rect.topleft[1] >= 1200:
@@ -332,8 +403,13 @@ def main():
             for play_sprite in play_sprites:
                 play_sprite.draw(data.screen)
                 play_sprite.update(clock.tick(FPS))
-            pressed = pygame.key.get_pressed()
-            horizontal = int(pressed[pygame.K_d]) - int(pressed[pygame.K_a])
+                trees.update(clock.tick(FPS), horizontal, ran_hero.down_flag)
+            if pressed[pygame.K_s]:
+                ran_hero.down_flag = True
+            elif not pressed[pygame.K_s]:
+                ran_hero.down_flag = False
+            count.update(clock.tick(FPS))
+            count.draw(data.screen)
             ran_hero.set_move(horizontal)
         all_sprites.update()
         all_sprites.draw(data.screen)
